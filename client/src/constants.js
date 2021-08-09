c0e7d8cb9fd05b1d066e87f72a4d4f85453efabf
Kyle Payne <kylepayne@numetric.com>
2021-08-09 15:57:05 -0600

    add constants

---
+++ client/src/constants.js
@@ -0,0 +1,15 @@
+const EVENTS = {
+  CONNECTION: 'connection', // client connect
+  DISCONNECT: 'disconnect', // client disconnect
+  JOIN: 'join', // user join room
+  LEAVE: 'leave', // user leaves room
+  CLOSE: 'close', // room close
+  DRAW: 'draw',
+  CHAT: 'chat',
+  CLEAR: 'clear',
+  UNDO: 'undo', // undo most recent draw
+};
+
+module.exports = {
+  EVENTS,
+};
